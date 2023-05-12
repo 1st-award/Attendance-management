f8e400ebf8baf8b78e5927e291e34189e503ace5
조송하 <102511282+Song-haJo@users.noreply.github.com>
2023-05-12 16:03:51 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,58 +16,8 @@ function startVideo() {
   }).catch(function (err0r) {
     console.log("Something went wrong!");
   });
-  //onPlay()
 }
 
-////////////////////////////////////////////////////////
-// Face-api 가이드 예제 코드
-/*
-async function onPlay() {
-  let tinyOptions = new faceapi.TinyFaceDetectorOptions({ inputSize: 128, scoreThreshold: 0.5 })
-  const result = await faceapi.detectAllFaces(video, tinyOptions).withFaceLandmarks().withFaceExpressions()
-  console.log('1')
-  if (result) {
-    const canvas = $('#overlay').get(0)
-    const dims = faceapi.matchDimensions(canvas, video, true)
-    faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
-  }
-
-  setTimeout(() => onPlay())
-}
-*/
-
-////////////////////////////////////////////////////////
-
-/*
-async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
-  imageUpload.addEventListener('change', async () => {
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(video.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
-  })
-}
-*/
 
 
 // face expression recognition model

7e88254a8bd37c37f76c9f29f3c8f016b6632680
Mooro <as4290156@gmail.com>
2023-05-12 14:56:29 +0900

    fix: 전반적 구조 변경

--- js/DB.js
+++ js/DB.js
@@ -1,11 +1,11 @@
 const db = window.openDatabase("database", "1.0", "database", 1024 * 1024);
 
 db.transaction((tx) => {
-    tx.executeSql("DROP TABLE attendance");
     tx.executeSql("CREATE TABLE attendance (no INTEGER PRIMARY KEY , subject TEXT NOT NULL , term INTEGER NOT NULL , week INTEGER NOT NULL , checked BOOLEAN NOT NULL CHECK ( checked IN(0, 1) ), checked_date TEXT NOT NULL)");
+    tx.executeSql("CREATE TABLE student (s_no INTEGER PRIMARY KEY , name TEXT NOT NULL)");
 });
 
-function add(subject, term, weeks, checked, checked_date) {
+function addAttendance(subject, term, weeks, checked, checked_date) {
     db.transaction((tx) => {
         tx.executeSql(`INSERT INTO attendance2 (subject, term, week, checked, checked_date)
                        VALUES ("${subject}", ${term}, ${weeks}, ${checked}, "${checked_date}")`, [], (tx, result) => {
@@ -16,7 +16,7 @@ function add(subject, term, weeks, checked, checked_date) {
     });
 }
 
-function getAll() {
+function getAllAttendance() {
     return new Promise((resolve) => {
         db.transaction((tx) => {
             tx.executeSql(`SELECT *
@@ -26,10 +26,3 @@ function getAll() {
         });
     });
 }
-
-getAll().then((result) => {
-    for (var i = 0; i < result.rows.length; i++) {
-        var row = result.rows.item(i);
-        console.log(row);
-    }
-});

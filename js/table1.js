d19001837ff91260c5a95c714df23738a7a03cd1
Bae Usuk <81358122+qodntjr@users.noreply.github.com>
2023-05-12 23:58:11 +0900

    Update table1.js

--- js/table1.js
+++ js/table1.js
@@ -3,7 +3,7 @@ for (i=0; i<50; i++){
   tc.push({
     name: "홍길동"+i,
     id: 200000+i,
-    img: "./labeled_images/Captain Marvel/1.jpg",
+    img: "./labeled_images/Captain Marvel/"+(i%2+1)+".jpg",
     weeks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     absence: 0
   });
